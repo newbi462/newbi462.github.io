6df4d062df3c5f10cd74f03e5cc884a7a849c22e
newbi462 <isthisemailfree@gmail.com>
2019-10-08 17:30:21 -0400

    LS forced portfolio-website chance to project site

--- global-theme/css-adds.php
+++
@@ -1,2 +0,0 @@
-    <link href="css/temps/svg-icon.css" rel="stylesheet">
-    <link href="css/temps/custom.css" rel="stylesheet">
--- global-theme/temp-css.php
+++
@@ -1,3 +0,0 @@
-    <link href="css/temps/album.css" rel="stylesheet">
-    <link href="css/temps/carousel.css" rel="stylesheet">
-    <link href="css/temps/product.css" rel="stylesheet">
