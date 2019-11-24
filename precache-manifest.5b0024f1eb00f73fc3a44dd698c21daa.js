9e83daaf0809b10f4e9ab314742563ac0ebf6f8b
newbi462 <isthisemailfree@gmail.com>
2019-11-24 12:45:15 -0500

    Updates

---
+++ precache-manifest.5b0024f1eb00f73fc3a44dd698c21daa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "172e192de270c69d9415912d7d910f99",
+    "url": "/react-american-football-scoreboard/index.html"
+  },
+  {
+    "revision": "0e538c1d59789b403b72",
+    "url": "/react-american-football-scoreboard/static/css/main.5bde505b.chunk.css"
+  },
+  {
+    "revision": "c246a0a3bdc68d8dd250",
+    "url": "/react-american-football-scoreboard/static/js/2.f7468746.chunk.js"
+  },
+  {
+    "revision": "0e538c1d59789b403b72",
+    "url": "/react-american-football-scoreboard/static/js/main.5aefc5b3.chunk.js"
+  },
+  {
+    "revision": "460ee2ee4ef1893464f5",
+    "url": "/react-american-football-scoreboard/static/js/runtime~main.baec3f42.js"
+  },
+  {
+    "revision": "1abd8c1a58598974d8415573764ba76a",
+    "url": "/react-american-football-scoreboard/static/media/Ozone.1abd8c1a.ttf"
+  }
+]);
