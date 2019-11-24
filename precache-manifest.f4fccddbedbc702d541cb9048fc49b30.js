07ab166074bc5afb0e9a1ed6430734170155cba6
newbi462 <isthisemailfree@gmail.com>
2019-11-24 14:03:40 -0500

    Updates

---
+++ precache-manifest.f4fccddbedbc702d541cb9048fc49b30.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "514d9ad638303839d33244e997308ad2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "991786159adaa9e43a62",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "659d98103a6ac7e27e0c",
+    "url": "/static/js/2.b4be4112.chunk.js"
+  },
+  {
+    "revision": "991786159adaa9e43a62",
+    "url": "/static/js/main.be8c3f94.chunk.js"
+  },
+  {
+    "revision": "33268800ac78d6aad712",
+    "url": "/static/js/runtime-main.eb3b6c26.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
