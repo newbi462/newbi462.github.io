ac6ceb0f9776525adfcc9494f10553ab56f1f713
newbi462 <isthisemailfree@gmail.com>
2019-12-25 10:41:39 -0500

    In progress todo Artifact

--- react-port/src/Components/ReactDemos.js
+++ react-port/src/Components/ReactDemos.js
@@ -3,7 +3,20 @@ import React, { useState } from "react";
 export const ReactDemos = () => {
   return (
     <>
-    test
+    <div className="tileBox reactBox">
+      <div
+        className="gridBox react-Selected"
+      >
+        <i className="fab fa-react w3-spin"></i>
+        <span>React</span>
+      </div>
+      <div className="gridBox make23box">
+        <iframe src="https://react-todo-drab-nu.now.sh/"></iframe>
+      </div>
+
+
+
+    </div>
     </>
   );
 };
